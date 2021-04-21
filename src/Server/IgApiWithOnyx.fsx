464f2947131b96926108d071f83d807ceacc34b3
bytesource <stefan.rohlfing@gmail.com>
2021-04-21 20:03:10 +0800

    Get initial authentication request working with Oryx

--- src/Server/IgApiWithOnyx.fsx
+++ src/Server/IgApiWithOnyx.fsx
@@ -3,32 +3,73 @@
 
 #load "Credentials.fs"
 open Credentials
-
-let api = apiKey
-
-//open System.Net.Http
-open System.Text.Json
-
-//open FSharp.Control.Tasks
-
-// open Oryx.SystemTextJson.ResponseReader
-open Oryx.ThothJsonNet.ResponseReader
-
+open Thoth.Json.Net
 open Oryx
+open Oryx.ThothJsonNet
+open System.Net.Http
+
+let credentials = Demo.igApi
+
+
 
 [<Literal>]
-let Url = "https://en.wikipedia.org/w/api.php"
+let Url = "https://demo-api.ig.com/gateway/deal/session/"
 
-let options = JsonSerializerOptions()
 
-let query term = [
-    struct ("action", "opensearch")
-    struct ("search", term)
-]
+let headers (ApiKey apiKey) (AccountId id) = 
+    [
+        ("VERSION", "3")
+        ("IG-ACCOUNT-ID", id)
+        ("X-IG-API-KEY", apiKey)
+       // ("Content-Type", "application/json; charset=UTF-8")
+        ("Accept", "application/json; charset=UTF-8")
+    ] 
+    |> Map.ofList
 
-let request term =
-    GET
+
+let client = new HttpClient()
+
+let context = 
+    HttpContext.defaultContext
+    |> HttpContext.withHttpClient client
+    |> HttpContext.withHeaders (headers credentials.Key credentials.Id)
+
+
+
+let encodeBody (Identifier identifier) (Password password) = 
+    Encode.object [
+        "identifier", Encode.string identifier
+        "password", Encode.string password
+    ]
+
+let contentBuilder = 
+    (fun () -> 
+        new JsonPushStreamContent(encodeBody credentials.Identifier credentials.Password) 
+        :> HttpContent)
+
+
+let request<'a> =
+    POST
     >=> withUrl Url
-    >=> withQuery (query term)
+    >=> withContent contentBuilder
     >=> fetch
-    >=> json options
+
+let resultTask = 
+    request
+    |> runAsync context
+
+let resultToString response = 
+    async {
+        let! r = response
+        match r with 
+        | Ok result -> return sprintf $"Response: {result}"
+        | Error msg -> return sprintf $"Error: {msg}"
+    }
+
+let authResponse = 
+    resultTask
+    |> Async.AwaitTask
+    |> resultToString
+    |> Async.RunSynchronously
+
+// "Response: System.Net.Http.HttpConnectionResponseContent"
